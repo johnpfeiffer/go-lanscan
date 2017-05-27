7fde4052249f5535fe3138f8789c04c15fa57eff
john pfeiffer <foupfeiffer@gmail.com>
2017-05-27 06:48:01 -0700

    attempt to improve the ui of the downloads page

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 #!/usr/bin/env python
 import webapp2
 
+downloads_html = """<html>
+<body>
+download the latest build at:
+<table>
+<tr>
+<th>os</th>
+<th>download link</th>
+</tr>
+<tr>
+<td>linux amd64</td>
+<td><a href='https://go-lanscan.appspot.com/go-lanscan'>https://go-lanscan.appspot.com/go-lanscan</a></td>
+</tr>
+</table>
+Source code at <a href='https://bitbucket.org/johnpfeiffer/go-lanscan'>https://bitbucket.org/johnpfeiffer/go-lanscan</a>
+</body>
+</html>
+"""
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('download the latest build at https://go-lanscan.appspot.com/go-lanscan')
+        self.response.write(downloads_html)
 
 app = webapp2.WSGIApplication([('/', MainHandler)], debug=True)
 
