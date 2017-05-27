9072cd0796c65c11a7dfb783ebf96b838ece0669
john pfeiffer <foupfeiffer@gmail.com>
2017-05-27 04:48:04 -0700

    setup a barebones app engine app (so the pipelines build is python for now)

---
+++ main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+import webapp2
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('hello')
+
+app = webapp2.WSGIApplication([('/', MainHandler)], debug=True)
+
